d181f1139cc38147fa3e96629c5876b112fe0c8b
chickienickie <nrs164@gmail.com>
2022-09-06 12:55:52 -0400

    Created dictionary app heading & css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary App</h1>
+      <h2>What word would you like to look up?</h2>
+    </div>
+  );
+}
+
+export default App;
