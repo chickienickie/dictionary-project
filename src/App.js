028aff7136acea87864bd5de7dc9ff153a0576e0
chickienickie <nrs164@gmail.com>
2022-09-06 21:07:34 -0400

    added background img to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
 import Typical from 'react-typical'
 import Dictionary from "./Dictionary"
+import "./App.css";
 
 
 const steps = [
-  'What word would you like to look up?', 2000,
+  'What word would you like to look up?', 1000,
 ];
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Dictionary App</h1>
-        <Typical steps={steps} loop={Infinity} />
+        <Typical steps={steps} loop={1} className="typical" />
         <main>
           <Dictionary />
         </main>
