8aeb337f3d245e5118f4fee45633d5a51d88aa50
chickienickie <nrs164@gmail.com>
2022-09-07 14:41:43 -0400

    Rendered the definition & word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -34,7 +36,7 @@ export default function Dictionary() {
         </span>
 
       </form>
-
+      <Results results={results} />
     </div>
   )
 }
