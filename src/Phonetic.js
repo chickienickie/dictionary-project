ac311cee653d8b5040bc4cb9c1a3ce68ce088544
chickienickie <nrs164@gmail.com>
2022-09-13 15:20:41 -0400

    added audio player to phonetic component

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,18 @@
 import React from "react";
-
 import "./Phonetic.css";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetic(props) {
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-      <br />
-      {props.phonetic.text}
+      <div className="audio-player">
+        <ReactAudioPlayer
+          src={props.phonetic.audio}
+          autoPlay={false}
+          controls
+        />
+      </div>
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
